648c587b0211d871e8492b97d9998e57a46417c6
HybridVenom <alexander.jonsson987@gmail.com>
2021-09-13 20:01:52 +0200

    how to read messages?

--- src/bot.js
+++ src/bot.js
@@ -9,6 +9,17 @@ client.login(process.env.BOT_TOKEN);
 
 client.on("ready", () => {
     console.log(`[i] ${client.user.tag} has logged in!`);
+    client.user.setActivity("your every move.", { type: "WATCHING"})
+
+    console.log(client.channels.cache.get("538373899109400608").messages)
+
+    /*
+    client.channels.cache.get("538373899109400608")
+        .fetchMessages({limit: 1})
+        .then(messages => {
+            console.log(`[${messages.first().author.name}]${messages.first().content}`)
+        });
+    */
 })
 
 function inPermittedChannel(channelName) {
@@ -16,6 +27,8 @@ function inPermittedChannel(channelName) {
 	if (config.permittedChannels.indexOf(channelName) > -1) return true;
 	else return false;
 }
+
+
 
 client.on("message", (message) => {
     try {
