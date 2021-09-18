c02475a6f20ed2c21ce9eeedbb5c5bfb6d7aef58
HybridVenom <alexander.jonsson987@gmail.com>
2021-09-18 20:52:11 +0200

    Bot now increasingly "wants" to reply
    On average responds to ~18% of messages now, some periods more some periods less..

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,7 @@ const { Client } = require("discord.js");
 // Global
 const ballquotes = ["don't count on it.", "as I see it, yes.", "it is certain.", "reply hazy, try again.", "my reply is no.", "most likely.", "it is decidedly so.", "ask again later.", "my sources say no.", "outlook good.", "without a doubt.", "better not tell you now.", "yes - definitely.", "cannot predict now.", "you may rely on it.", "concentrate and ask again.", "outlook not so good.", "signs point to yes.", "very doubtful.", "yes."];
 let markovMessage;
+let boundary = 0.10;
 
 // Init
 const client = new Client();
@@ -54,9 +55,15 @@ client.on("message", (message) => {
                     break;
                 default:
                     let rand = Math.random();
-                    console.log("Rolled: ", rand);
-                    if (rand <= 0.10)
+                    console.log("Rolled (boundary): ", rand, "("+boundary+")");
+                    if (rand <= boundary) {
+                        boundary -= 0.20;
+                        if (boundary < 0.0)
+                            boundary = 0.0
                         setTimeout(function(){message.reply(getMarkovMessage())}, 500)
+                    }
+                    else
+                        boundary += 0.04;
                     break;
 			}
 		}
